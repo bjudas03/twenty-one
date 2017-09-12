ef7204affc794cca5ad552c66fecf4e4b51d42e8
bjudas03 <ben_judas@hotmail.com>
2017-09-11 21:12:51 -0700

    second commit

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-// Known needed functions
-// Randomize Deck
-//		USE API to get deck object
-// 	deal first two cards to each player
-// Bet function toPot
-// Draw function = pull 1 card from deck and add to UL array of player/dealer
-// 	calculate value of card and add to value of cards in player array
-// 	if value is over 21 = player/dealer busts
-// Bet function = input box w/ value of bet -> add to center toPot
-// Bust = if player card value =< 21, player turn over and pot goes to opposing player
-// Call function = player forfeits turn -> dealer continues
+//Initial Deal (same as Draw Card function, but change 'count=1' to 'count=2' in url)
+	//set as a function that will run on board set-up and reset
+	//display vlue of drawn cards in 
+
+//BET pseudocode
+	//input player $ value into text box
+	//pres commit
+		//value from text box get removed from player pot
+		//value from text box gets added to betting pot
+		//switch turn to player 2
+	//input dealer $ value into text box
+		//value from text box gets removed from dealer pot
+		//value from text box gets added to betting pot
+		//swtich to player 1
+		//start draw card function
+
+//Draw Card Function
+//  --  needs  --  
+		//pull 1 card from API 
+		//parse string value of card to integer: add to player score
+			//check to see if score value => 21
+				//if 21 => you win
+				//if over 21 => you bust
+				//if under 21
+				//	reset back to draw card function
+	//option for draw or stay
+		//on draw - repeat from draw card function
+		//on stay - next turn
+			//set interval 5 secs
+			//hide player section
+			//switch turn
+
+//switch turn function
+	//increase turn counter ++
+	//show player section
+
+
+
+
+
 
 var url = 'https://deckofcardsapi.com/api/deck/';
 var deckId = '';
-var cardValue = //the card.value comes as a string from the AJAX object- Use parseInt() to convert to integer
+var cardValue;//the card.value comes as a string from the AJAX object- Use parseInt() to convert to integer
+// var game = loadDeck();
+var cardObject;
 
 // var cardArray = [];
 // var dealerArray =[];
@@ -29,21 +60,52 @@ var cardValue = //the card.value comes as a string from the AJAX object- Use par
 // var playerScore = 0
 // var dealerScore = 0
 
-// $('#dealer-bank').push(dealerBank);
-// $('#player-bank').push(playerBank);
+//gets deck from API
+// function loadDeck() {
+$.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').done(function(response) {
+	// console.log(response);
+	deckId = response.deck_id;
+	localStorage.deck = deckId;
+});	
+deckId = localStorage.deck;
+console.log(deckId)
+
+function initiateGame() {
+	console.log('inside the initiateGame ', deckId)
+	$.get('https://deckofcardsapi.com/api/deck/'+deckId+'/draw/?count=2').done(function(response) {
+		console.log(response);
+		cardObject = response;
+		// console.log('draw works');
+		for (value in cardObject) {
+			for (i = 0; i<=cardObject.length; i++) {
+			cardValue = parseInt(cardObject[i].value);
+			console.log(cardValue);
+		}
+	}
+		// for (image in cardObject) {
+		// 		$("#player-cards").append("<img class='cardImage' src=" + cardObject.image + "/>");
+		// 		console.log("test")
+			// 	console.log("image tag works");
+			// for (value in obj) {
+			// 	var value = parseInt(obj.value);
+			// 	$(#)
+
+			// }
+		// }
+		}
+	)
+};
+//////Do other query here, your deckId is localStorage.deck
+
+// function drawCard(){
+// 	console.log('inside of draw card', deckId)
+// 	$.get('https://deckofcardsapi.com/api/deck/'+deckId+'/draw/?count=1').done(function(response) {
+// 		console.log('draw works');
+// 		console.log(response);
+// 	}
+// )};
+
+initiateGame();
+// drawCard();
 
 
-function newDeck() {
-	var deck = $.get('https://deckofcardsapi.com/api/deck/new/shuffle', {
-}).done(function(response) {
-	console.log(response);
-});
-}
-
-
-
-
-
-// function createBoard() {
-
-// }
