07d87726c7e10b55046c30f45493309863f582be
bjudas03 <ben_judas@hotmail.com>
2017-09-14 13:53:34 -0700

    commit

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//Initial Deal (same as Draw Card function, but change 'count=1' to 'count=2' in url)
-	//set as a function that will run on board set-up and reset
-	//display vlue of drawn cards in 
-
 //BET pseudocode
 	//input player $ value into text box
 	//pres commit
@@ -39,64 +35,56 @@
 
 
 
-var url = 'https://deckofcardsapi.com/api/deck/';
 var deckId = '';
-var cardValue;//the card.value comes as a string from the AJAX object- Use parseInt() to convert to integer
-// var game = loadDeck();
+var cardValue;
 var cardObject;
-
-// var cardArray = [];
-// var dealerArray =[];
-// var playerArray =[];
-
-// var dealerBank = 1000
-// // var dealerBet = ?
-// var playerBank = 1000
-// // var playerBet = ?
-
-// var pot = playerBet + dealerBet;
+var playerBank = 1000
+var pot = 0;
 
 // var turnCount = 0
-var playerScore;
-var dealerScore;
+var playerScore = 0;
+var dealerScore = 0;
 
-//gets deck from API
-// function loadDeck() {
+
+//loads deck and stores in local memory
 $.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').done(function(response) {
-	// console.log(response);
 	deckId = response.deck_id;
 	localStorage.deck = deckId;
 });	
 deckId = localStorage.deck;
 console.log(deckId)
 
-// function initiateGame() {  //can possibly just use the drawCard() function twice, switch player, and run  twice more.
-// 	console.log('inside the initiateGame ', deckId)
-// 	$.get('https://deckofcardsapi.com/api/deck/'+deckId+'/draw/?count=2').done(function(response) {
-// 		console.log(response);
-// 		cardObject = response.cards;
-// 		console.log(cardObject);
-		// console.log('draw works');
-		// for (value in cardObject) {
-		// 	for (i = 0; i<=cardObject.length; i++) {
-		// 	cardValue = parseInt(cardObject[i].value);
-		// 	console.log(cardValue);
-		// }
-	// }
-		// for (image in cardObject) {
-		// 		$("#player-cards").append("<img class='cardImage' src=" + cardObject.image + "/>");
-		// 		console.log("test")
-			// 	console.log("image tag works");
-			// for (value in obj) {
-			// 	var value = parseInt(obj.value);
-			// 	$(#)
 
-			// }
-		// }
-// 		}
-// 	)
-// };
-//////Do other query here, your deckId is localStorage.deck
+
+//////Game Functions
+function bet() {
+	var playerBet = $("#player-value").val();
+	playerBet = parseInt(playerBet);
+	if (playerBet <= playerBank) {
+			$("#player-bank").text(parseInt(playerBank));
+				console.log(playerBet);
+				$("#player-value").val('');  //resets the player bet box
+				$("#player-bank").text(parseInt(playerBank) - parseInt(playerBet)); //returns the player bank as less the bet: returns as string??
+				playerBank = playerBank - parseInt(playerBet);
+				console.log(playerBank);
+				pot = parseInt(playerBet) + parseInt(pot); //should add player bet to total pot - returns as Object object??
+				console.log(pot);
+				$("#betting-pot").text(parseInt(pot)); //puts new total pot in betting pot <p<span>> tag
+	} else {
+		function alert() {
+			alert("Please Make an Acceptable Bet");  //make popup window with message "please make an acceptable bet"
+			};
+	}	
+};
+
+//function to draw initial cards: draw card function 2x/switch player/drawcard2x/switchplayer
+function startGame() {
+	//clear pot
+	//clear player points
+	//clear playecards div
+	drawCard();
+	drawCard();
+};
 
 //////how to make this function specific to individual player?? make images/values push to correct divs
 function drawCard(){
@@ -113,44 +101,111 @@ function drawCard(){
 					case "KING":
 					case "QUEEN":
 					case "JACK":
-						return 10;
-						console.log(cardObject[0].value);
-						playerScore = cardObject[0].value.return + playerScore;
+						playerScore = playerScore + 10;
 						break;
 					case "ACE": 
 						if ((playerScore + 11) <= 21) {
-							return 11;
-						} else if ((playerScore + 1) <= 21) {
-							return 1;
+							playerScore = playerScore + 11;
+						} else if ((playerScore + 11) > 21) {
+							playerScore = playerScore + 1;
 						}
-						playerScore = cardObject[0].value.return + playerScore;
-						console.log(cardObject[0].value);
 						break;
 					default:	
 						var valueString = cardObject[0].value;
 						cardValue = parseInt(valueString);
 						console.log(cardValue);
-						playerScore = playerScore + cardValue
+						playerScore = playerScore + cardValue;
 						break;
 				}
-			  ////set return value of cardObject[0].value to new variable
-			  //add existing score to new variable = new score
-			} //get element by id (span/player-points) and set text to new score
-		});
-	});	
+			$('#player-points').text(playerScore)}; //get element by id (span/player-points) and set text to new score
+		});//checkWinner - check to see if player busts
+	});	//callButton() - switch to dealer turn
 }		
 
+// Dealer Turn  -  reasign tags for dealer side
+//pseudocode - 
+	//call api
+	//store result in temp array
+	//lay down cards[0] and cards[1]
+	//figure out value for cards[0],[1]
+	//while loop - dealerScore <= 16 {
+			//then hit
+		//} else {
+			//stand
+		//}
 
 
-// initiateGame() {
-// 	drawCard();
-// 	drawCard();
-// }
-drawCard();
+function dealerTurn() {
+	$.get('https://deckofcardsapi.com/api/deck/'+deckId+'/draw/?count=7').done(function(response) {
+		cardArray = response.cards;
+		console.log(cardArray);
+		// for (var i = 0; i <= cardArray.length) {
+		var i = 0;
+		var currentCardIndex = 0;
+		console.log(currentCardIndex);
+			
+			while (dealerScore <= 16) {
+				cardArray[currentCardIndex]   
+					var image = $('<img>').attr('src', cardArray[currentCardIndex].image);
+						image.attr('class', 'cardImage');
+						$("#dealer-cards").append(image);
+
+					for (value in cardArray[currentCardIndex]) { //process is getting stuck in this loop
+						switch (cardArray[currentCardIndex].value) {
+							case "KING":
+							case "QUEEN":
+							case "JACK":
+								dealerScore = dealerScore + 10;
+								break;
+							case "ACE": 
+								if ((dealerScore + 11) <= 21) {
+									dealerScore = dealerScore + 11;
+								} else if ((dealerScore + 11) > 21) {
+									dealerScore = dealerScore + 1;
+								}
+								break;
+							default:	
+								var valueString = cardArray[currentCardIndex].value;
+								cardValue = parseInt(valueString);
+								console.log(cardValue);
+								dealerScore = dealerScore + cardValue;
+								break;
+						}
+					$('#dealer-points').text(dealerScore);
+					break;
+						 //get element by id (span/player-points) and set text to new score
+					}//if playerscore > 21 - reset pot - restart game()
+				currentCardIndex++;
+			}
+		}
+	)};			
+// };
+
+
+
+
+
+
+
+
+//call button
+	//dealerDraw()
+	//checkfor Bust
+	//checkWin
+	//relocatePot
+$(".call").on('click', function() {
+	dealerTurn();
+});
+
+debugger;
+dealerTurn();
+bet();
+$(".commit").on('click', bet);
 
 //does this click instigate other actions? Maybe add to draw function.
 $(".draw").on('click', function(){
 	drawCard();
 });
+
 
 
